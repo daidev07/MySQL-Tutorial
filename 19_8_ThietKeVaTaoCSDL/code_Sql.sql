ecaae8a345ffc7cf304e9384f7d96026ece9e71e
daidev07 <hoxuandai02@gmail.com>
2024-08-20 21:27:53 +0700

    BT: Thiet ke va tao CSDL - Ngay 19/8

---
+++ 19_8_ThietKeVaTaoCSDL/code_Sql.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS mysqltutorial;
+USE mysqltutorial;
+
+-- Tạo bảng Tác Giả
+CREATE TABLE tac_gia (
+    id_tac_gia INT PRIMARY KEY AUTO_INCREMENT,
+    ten VARCHAR(255) NOT NULL
+);
+
+-- Tạo bảng Sách
+CREATE TABLE sach (
+    id_sach INT PRIMARY KEY AUTO_INCREMENT,
+    ten VARCHAR(255) NOT NULL,
+    gia DECIMAL(10, 2) NOT NULL,
+    id_tac_gia INT,
+    FOREIGN KEY (id_tac_gia) REFERENCES tac_gia(id_tac_gia)
+);
+
+-- Tạo bảng Nhân Viên
+CREATE TABLE nhan_vien (
+    id_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
+    ten VARCHAR(255) NOT NULL,
+    dia_chi TEXT NOT NULL
+);
+
+-- Tạo bảng quan hệ giữa Nhân Viên và Sách
+CREATE TABLE nhan_vien_ban_sach (
+    id_nhan_vien INT,
+    id_sach INT,
+    PRIMARY KEY (id_nhan_vien, id_sach),
+    FOREIGN KEY (id_nhan_vien) REFERENCES nhan_vien(id_nhan_vien),
+    FOREIGN KEY (id_sach) REFERENCES sach(id_sach)
+);
